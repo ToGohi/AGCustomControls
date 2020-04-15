5542eb82f9e0c8ee39af86f6841f838f94ffc6ef
Antonio González <togohi@gmail.com>
2020-04-15 14:33:38 +0200

    ADD AGCustomImageView

---
+++ AGCustomControls/AGCustomImageView.swift
@@ -0,0 +1,9 @@
+//
+//  AGCustomImageView.swift
+//  AGCustomControls
+//
+//  Created by Antonio Gonzalez Hidalgo on 15/04/2020.
+//  Copyright © 2020 Antonio Gonzalez Hidalgo. All rights reserved.
+//
+
+import Foundation
